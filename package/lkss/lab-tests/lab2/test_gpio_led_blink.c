d355013986431fae7b69c7939b52a49f6ace45c8
Iuliana Prodan <iuliana.prodan@nxp.com>
2025-07-04 20:30:48 +0300

    package: lkss: add lab2 test sources
    Add test sources for Lab2, for each exercise.
    
    Signed-off-by: Iuliana Prodan <iuliana.prodan@nxp.com>

---
+++ package/lkss/lab-tests/lab2/test_gpio_led_blink.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main() {
+	int fd = open("/dev/gpio_led", O_WRONLY);
+	if (fd < 0) {
+		perror("open");
+		return 1;
+	}
+
+	printf("Turning LED ON...\n");
+	write(fd, "1", 1);
+	sleep(1);
+	printf("Turning LED OFF...\n");
+	write(fd, "0", 1);
+	sleep(1);
+	printf("Blinking LED...\n");
+	write(fd, "b", 1);
+	sleep(7);
+	printf("Turning LED OFF...\n");
+	write(fd, "0", 1);
+	printf("Done...\n");
+
+	close(fd);
+	return 0;
+}
--- package/lkss/lab-tests/lab2/test_reverse_char.c
+++ package/lkss/lab-tests/lab2/test_reverse_char.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <string.h>
-#include <unistd.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<errno.h>
+#include<fcntl.h>
+#include<string.h>
+#include<unistd.h>
 
 #define BUFFER_LENGTH 256		// The buffer length (crude but fine)
 static char receive[BUFFER_LENGTH];	// The receive buffer from the LKM
@@ -14,7 +14,7 @@ int main()
 	char stringToSend[BUFFER_LENGTH];
 
 	printf("Starting device test code example...\n");
-	fd = open("/dev/simple_char", O_RDWR);	// Open the device with read/write access
+	fd = open("/dev/reverse_char", O_RDWR);	// Open the device with read/write access
 	if (fd < 0) {
 		perror("Failed to open the device...");
 		return errno;
@@ -38,7 +38,7 @@ int main()
 
 	printf("Reading from the device...\n");
 	ret = read(fd, receive, BUFFER_LENGTH);	// Read the response from the LKM
-	if (ret < 0){
+	if (ret < 0) {
 		perror("Failed to read the message from the device.");
 		return errno;
 	}
